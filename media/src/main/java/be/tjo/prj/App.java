c77d68e95758e0745db9c00a8f95668ecd9bf1a6
Peter Sinjan <peter.sinjan@gmail.com>
2011-07-08 18:44:55 +0200

    Initial project setup

---
+++ media/src/main/java/be/tjo/prj/App.java
@@ -0,0 +1,13 @@
+package be.tjo.prj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ media/src/test/java/be/tjo/prj/AppTest.java
@@ -0,0 +1,20 @@
+package be.tjo.prj;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() throws Exception {
+        assertTrue(true);
+    }
+}
+
+
